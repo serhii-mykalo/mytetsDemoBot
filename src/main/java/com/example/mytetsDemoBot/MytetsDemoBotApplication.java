ac6befd98dd3565fa6bfef21a998247fb26efd55
Serhii Mykalo <serhii.mask@gmail.com>
2025-02-07 17:56:40 +0200

    init

---
+++ src/main/java/com/example/mytetsDemoBot/MytetsDemoBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytetsDemoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytetsDemoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytetsDemoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mytetsDemoBot/MytetsDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytetsDemoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytetsDemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
